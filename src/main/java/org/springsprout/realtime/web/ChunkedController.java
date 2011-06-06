85add95239268f71155bb66db8a3333dd0a463f4
isYoon <helolsjava@gmail.com>
2011-06-05 17:33:15 -0700

    chunked example complated.

---
+++ src/main/java/org/springsprout/realtime/web/ChunkedController.java
@@ -0,0 +1,64 @@
+/**
+ * ©2011 Looah, LLC
+ * jco-2011-realtime
+ * created by isyoon Jun 5, 2011 2:17:47 PM
+ */
+package org.springsprout.realtime.web;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springsprout.realtime.support.ChatStore;
+
+@Controller
+public class ChunkedController {
+    @Autowired
+    ChatStore chatStore;
+    ObjectMapper om = new ObjectMapper();
+    
+    @RequestMapping("/chatListener")
+    public void chatListener(HttpServletRequest req, HttpServletResponse res, @RequestParam()String uuid) throws Exception {
+        ServletOutputStream out = res.getOutputStream();
+        try {
+            res.setContentType("application/json; charset=utf-8;");
+            res.setHeader("Connection", "close");
+            chatStore.enterStore(uuid);
+            JsonGenerator generator = om.getJsonFactory().createJsonGenerator(res.getOutputStream(), JsonEncoding.UTF8);
+            while(true){
+                om.writeValue(generator,chatStore.getData(uuid));
+                synchronized (this) {
+                    wait();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            chatStore.remveStore(uuid);
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+    
+    @RequestMapping("/sendMsg")
+    public ModelAndView sendMsg(@RequestParam()String name, @RequestParam()String msg, Model model){
+        model.addAttribute("isSuccess",chatStore.putMessage(name, msg));
+        synchronized (this) {
+           notifyAll();
+        }
+        return new ModelAndView("mappingJacksonJsonView");
+    }
+}
--- src/main/webapp/js/9981.js
+++ src/main/webapp/js/9981.js
@@ -1,30 +1,72 @@
 jQuery(function($) {
-  var xhr =  new XMLHttpRequest();
-  var startIdx = 0;
-  xhr.open('GET', 'chat', true);
-  xhr.onreadystatechange = function(e) {
-    console.log(xhr.readyState)
-     if (xhr.readyState == 3 && xhr.status == 200) {
-       console.log(e);
-     }
-  };
-  xhr.onprogress = function(e){
-      console.log(e.target.responseText.substring(startIdx,e.loaded));
-      startIdx += (e.loaded-startIdx);
-  };
-  xhr.send(null);
-
-  var sendMsg = function(who, msg) {
-    if (msg) {
-      $('div#chatWindow').append('<p> ' + who + ' &gt; ' + msg + '</p>').find(
-          'p:last-child')[0].scrollIntoView(true);
-    }
-  };
-
+  var name = prompt('What is your name?');
+  if(!name || !$.trim(name)){
+    name = 'What the hell?';
+  }
+  
+  var uuid = name+(new Date().getTime()),
+      sendMsg = function(msg) {
+        if($.trim(msg)){
+          $.post('sendMsg',{'name':name,'msg':msg},function(data){
+            if(!data.isSuccess){
+              alert('Message sending failed.');
+            }
+          },'json');
+        }
+      },
+      updateChat = function(data){
+        if(data.state !== 'wait'){
+          $('#chatWindow').append('<p><b>'+data.name+' &gt </b>'+data.msg+'</p>');
+          $('#chatWindow p:last-child')[0].scrollIntoView(true);
+        }
+      },
+      
+      chunkedReader = (function(){
+        var xhr =  new XMLHttpRequest(),
+            chunkIdx = 0,
+            responseText = '',
+            isDone = false;
+        
+        xhr.open('GET', 'chatListener?uuid='+uuid, true);
+        
+        xhr.onreadystatechange = function(e) {
+          if ((xhr.readyState == 3 || xhr.readyState == 4) && xhr.status == 200 && !isDone) {
+             responseText = xhr.responseText;
+             responseText = responseText.substring(chunkIdx);
+             chunkIdx += responseText.length;
+             if(responseText === ''){
+               isDone = true;
+             }
+             if(isJSONString(responseText)){
+               updateChat($.parseJSON(responseText));
+             }
+           }
+          if(isDone){
+            xhr.abort();
+          }
+        };
+        xhr.send(null);
+      })(),
+      
+      isJSONString = function( text ){
+        if(!text){
+          return false;
+        }
+        return (/^[\],:{}\s]*$/).test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
+                          .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+                          .replace(/(?:^|:|,)(?:\s*\[)+/g, '')); 
+      };
+  
   $('input.msg').keydown(function(e) {
     if (e.keyCode === 13 || e.ekyCode === 10) {
-      sendMsg('me', $(this).val());
+      sendMsg($(this).val());
       $(this).val('');
     }
   }).focus();
+  
+  $('div.inAction > button').click(function(){
+    sendMsg($(this).prev().val());
+    $(this).prev().val("").focus();
+  });
+  
 });
