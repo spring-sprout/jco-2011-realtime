816028b68333a21d7789e7ecb9510e2f8f03dfd4
Outsider <outsideris@gmail.com>
2011-06-11 03:36:57 +0900

    문제 정리

--- src/main/java/org/springsprout/realtime/oxquiz/QuizStore.java
+++ src/main/java/org/springsprout/realtime/oxquiz/QuizStore.java
@@ -10,12 +10,12 @@ public class QuizStore {
     
     static {
         Set<String> quizSet = new HashSet<String>();
-        //quizSet.add("Firefox 4는 기본적으로 Web Socket을 지원한다.");
         quizSet.add("자바에서 어느 서블릿 컨테이너가 Comet을 지원하는지 알고 있다.");
-        quizSet.add("웹소켓을 사용해 본 적이 있다.");
-        quizSet.add("각 Comet 기술들의 동작방식을 설명할 수 있다.");
+        quizSet.add("웹소켓을 사용해 본적이 있다.");
         quizSet.add("Comet을 사용하는 서비스를 개발한적이 있다.");
-        //quizSet.add("Comet이라는 용어는 Alex Russell이 만들었다.");
+        quizSet.add("각 Comet 기술들의 동작방식을 설명할 수 있다.");
+        quizSet.add("Comet의 여러가지 기술들에 대한 기본적인 개념들을 알고 있다.");
+        quizSet.add("리얼타입웹 기술들에 대해서 들어본 적이 있다.");
         
         quizs = quizSet.iterator();
     }
